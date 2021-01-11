ab750b16e7209d3eb9b763f604d3aa8087e1f122
Mikołaj Buczak <spam.mikolaj.buczak@gmail.com>
2021-01-11 10:45:47 +0100

    Added navigation file

--- Rejestrator/app/src/main/java/com/example/rejestrator/view/MainActivity.kt
+++ Rejestrator/app/src/main/java/com/example/rejestrator/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.rejestrator
+package com.example.rejestrator.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rejestrator.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
